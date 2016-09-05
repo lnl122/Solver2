1c29f89dfe9f4bd28764fee511b12bfdd66c9630
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-05 17:54:54 +0600

    again new name

--- Solver2/Solver2/Form1.Designer.cs
+++ Solver2/Solver2/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Name = "Form1";
-            this.Text = "new name of app";
+            this.Text = "ultra new name of app";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
