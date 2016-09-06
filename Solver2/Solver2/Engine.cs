f6fa3e959c046600a3d8e5cea1c2fd117d715dd3
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-06 14:12:02 +0600

    try-catch in get_page

--- Solver2/Solver2/Engine.cs
+++ Solver2/Solver2/Engine.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Solver2
 {
@@ -16,6 +11,37 @@ namespace Solver2
         private static string cHead;            // куки
         private static CookieContainer cCont;   // куки
 
+
+        // получает страницу по урлу
+        public static string get_page(string url)
+        {
+            string ps = "";
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(url);
+            //getRequest.Headers.Add("Accept-Language", "ru-ru,ru");
+            //getRequest.Headers.Add("Content-Language", "ru-ru,ru");
+            ////getRequest.Headers.Set("Accept-Charset", "utf-8");
+            ////getRequest.Headers.Set("Accept-Encoding", "utf-8");
+            ////getRequest.Headers.
+            ////Accept-Charset
+            ////Accept-Encoding
+            //getRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1";
+            try
+            {
+                getRequest.CookieContainer = cCont;
+                WebResponse getResponse = getRequest.GetResponse();
+                using (StreamReader sr = new StreamReader(getResponse.GetResponseStream()))
+                {
+                    ps = sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+                Log.Write("gpage ERROR: Не удалось прочитать страницу ", url);
+                ps = "";
+            }
+            return ps.ToLower();
+        }
+        
         // выполняем логон в движке
         // вход - урл, логин, пасс
         // выход - страница с ответом
@@ -23,7 +49,7 @@ namespace Solver2
         {
             string formParams = string.Format("Login={0}&Password={1}", name, pass);
             string cookieHeader = "";
-            var cookies = new CookieContainer();
+            CookieContainer cookies = new CookieContainer();
             cCont = cookies;
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url1);
             req.CookieContainer = cookies;
