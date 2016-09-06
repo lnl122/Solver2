71c20bbb14fd43e8f91ea898bc364e408b5963af
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-06 10:16:23 +0600

    add Log module

---
+++ Solver2/Solver2/Log.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Solver2
+{
+    // public void Init()
+    // public void Close()
+    // public void Write(string text, string text2="", string text3 = "")
+    // public void Store(string modulename, string pagetext)
+    //
+    class Log
+    {
+        //private static string PathToLogs = "";          // путь (без файла) к логам
+        private static string PathToPages = "";         // путь (без слеша в конце, к папке для сохраняемых страниц
+        private static System.IO.StreamWriter logfile;  // поток лога
+        public static bool isReady = false;             // инициализация проведена?
+        private static bool isBusy = false;             // счас заняты? чтоб подождать если необходимо. для устранения коллизий при активном логгировании
+        private static int fileidx = 1;                 // индекс/номер сохраняемого файла
+
+        // записывает строку текста в лог-файл
+        // вход     строка для лог файла
+        // выход    -
+        public static void Write(string str, string str2 = "", string str3 = "")
+        {
+            if (isReady)
+            {
+                while (isBusy) { } // ожидание освобождения флага
+                isBusy = true;
+                logfile.WriteLine("{0} {1} {2}", DateTime.Today.ToShortDateString(), DateTime.Now.ToLongTimeString(), str);
+                if (str2 != "")
+                {
+                    logfile.WriteLine("                          " + str2);
+                    if (str3 != "")
+                    {
+                        logfile.WriteLine("                          " + str3);
+                    }
+                }
+                isBusy = false;
+            }
+        }
+
+        // записывает текст в отдельный файл
+        // вход     имя модуля, строка текста
+        // выход    -
+        public static void Store(string modulename, string text)
+        {
+            if (isReady)
+            {
+                var dt = DateTime.Today;
+                var dn = DateTime.Now;
+                string path = PathToPages + "\\" + modulename + "_" + fileidx.ToString() + "_" +
+                    dt.Year.ToString() + dt.Month.ToString() + dt.Day.ToString() +
+                    dn.Hour.ToString() + dn.Minute.ToString() + dn.Second.ToString() + ".http";
+                System.IO.File.WriteAllText(path, text, System.Text.Encoding.UTF8);
+                fileidx++;
+            }
+        }
+
+        // выполняет принудительную запись лога на диск
+        // вход     -
+        // выход    -
+        public static void Close()
+        {
+            if (isReady)
+            {
+                logfile.Flush();
+                logfile.Close();
+                logfile = null;
+                isReady = false;
+            }
+        }
+
+        // если папка есть, или если не было, но удалось создать - возвращает путь к ней, иначе - базовый путь
+        // вход     базовый путь, имя папки
+        // выход    путь к папке
+        private static string CheckCreateFolder(string basepath, string folder)
+        {
+            string path = basepath + @"\" + folder;
+            if (System.IO.Directory.Exists(path) == false)
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                catch
+                {
+                    path = basepath;
+                }
+            }
+            return path;
+        }
+
+        // инициализирует лог файл, если нету его - создает. в т.ч. необходимые папки
+        // вход     -
+        // выход    -
+        public static void Init()
+        {
+            string local_path = Environment.CurrentDirectory;
+            string self_name = System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName;
+            string PathToLogs = CheckCreateFolder(local_path, "Log");
+            string PathToData = CheckCreateFolder(local_path, "Data");
+            PathToPages = CheckCreateFolder(local_path, "Pages");
+            string pathfilename = PathToLogs + "\\" + self_name + ".log";
+            logfile = new System.IO.StreamWriter(System.IO.File.AppendText(pathfilename).BaseStream);
+            logfile.AutoFlush = true;
+            isReady = true;
+        }
+    }
+}
--- Solver2/Solver2/Program.cs
+++ Solver2/Solver2/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,9 +12,161 @@ namespace Solver2
 {
     static class Program
     {
-        static void Main()
+
+        // получаем строку с версиями установленных .net
+        private static string GetVersionDotNetFromRegistry()
         {
-            MessageBox.Show("123");
+            string res = "";
+            using (RegistryKey ndpKey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+                        res = res + versionKeyName + " ";
+                    }
+                }
+            }
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+            {
+                if (ndpKey != null && ndpKey.GetValue("Release") != null)
+                {
+                    int releaseKey = (int)ndpKey.GetValue("Release");
+                    if (releaseKey >= 393295) { res = res + " v4.6"; }
+                    else
+                    {
+                        if ((releaseKey >= 379893)) { res = res + " v4.5.2"; }
+                        else
+                        {
+                            if ((releaseKey >= 378675)) { res = res + " v4.5.1"; }
+                            else
+                            {
+                                if ((releaseKey >= 378389)) { res = res + " v4.5"; }
+                            }
+                        }
+                    }
+                }
+            }
+            return res.Trim();
+        }
+        // получаем строку с версией MS Word
+        private static string GetVersionMicrosoftWord()
+        {
+            try
+            {
+                var WordApp = new Microsoft.Office.Interop.Word.Application();
+                string s1 = WordApp.Version;
+                WordApp.Quit();
+                return s1;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        // проверяем наличие, настройки и также работу всех необходимых компонент, ведем лог
+        private static bool CheckComponents()
+        {
+            // .NET
+            string DotNetVersions = GetVersionDotNetFromRegistry();
+            Log.Write("check Найденные версии .NET: " + DotNetVersions);
+            if (DotNetVersions.IndexOf("v2.0") == -1) { Log.Write("check ERROR: Отсутствует .NET v2.0"); return false; }
+            if (DotNetVersions.IndexOf("v3.0") == -1) { Log.Write("check ERROR: Отсутствует .NET v3.0"); return false; }
+            if (DotNetVersions.IndexOf("v4.0") == -1) { Log.Write("check ERROR: Отсутствует .NET v4.0"); return false; }
+            if ((DotNetVersions.IndexOf("v4.5") == -1) && (DotNetVersions.IndexOf("v4.6") == -1)) { Log.Write("check ERROR: Отсутствует .NET v4.5 или v4.6"); return false; }
+
+            // MS Word
+            string WordVersion = GetVersionMicrosoftWord();
+            if (WordVersion == "") { Log.Write("check ERROR: Отсутствует установленный Microsoft Word"); return false; }
+            int ii1 = 0;
+            if (Int32.TryParse(WordVersion.Substring(0, WordVersion.IndexOf(".")), out ii1))
+            {
+                if (ii1 <= 11) { Log.Write("check ERROR: Версия Microsoft Word ниже 11.0, необходим Microsoft Word 2007 или более новый"); return false; }
+            }
+            else
+            {
+                Log.Write("check ERROR: Не удалось определить версию Microsoft Word"); return false;
+            }
+            Log.Write("check Найден Microsoft Word версии " + WordVersion);
+            /*try
+            {
+                var testSC = new SpellChecker();
+                if (testSC.CheckOne("мама") && testSC.CheckOne("мыла") && testSC.CheckOne("раму"))
+                {
+                    Log.Write("check Проверка орфографии установлена");
+                }
+            }
+            catch
+            {
+                Log.Write("ERROR: Не удалось запустить проверку орфографии, или же проверка русского языка не установлена.."); return false;
+            }*/
+
+            // проверка открытия web-ресурсов
+            WebClient wc1 = null;
+            try { wc1 = new WebClient(); } catch { Log.Write("check ERROR: Не удалось создать объект WebClient"); return false; }
+            string re1 = "";
+            try { re1 = wc1.DownloadString("http://image.google.com/"); } catch { Log.Write("check ERROR: http://image.google.com/ не открывается"); return false; }
+            try { re1 = wc1.DownloadString("http://game.en.cx/"); } catch { Log.Write("check ERROR: http://game.en.cx/ не открывается"); return false; }
+            //try { re1 = wc1.DownloadString("http://jpegshare.net/"); }      catch { Log.Write("check ERROR: http://jpegshare.net/ не открывается");     return false; }
+            //try { re1 = wc1.DownloadString("http://ipic.su/"); }            catch { Log.Write("check ERROR: http://ipic.su/ не открывается");           return false; }
+            try { re1 = wc1.DownloadString("http://goldlit.ru/"); } catch { Log.Write("check ERROR: http://goldlit.ru/ не открывается"); return false; }
+            try { re1 = wc1.DownloadString("http://sociation.org/"); } catch { Log.Write("check ERROR: http://sociation.org/ не открывается"); return false; }
+            try { re1 = wc1.DownloadString("https://ru.wiktionary.org/"); } catch { Log.Write("check ERROR: https://ru.wiktionary.org/ не открывается"); return false; }
+            Log.Write("check Все необходимые web-ресурсы открываются успешно");
+
+            // все проверки пройдены
+            return true;
+        }
+
+        // инициализируем наши объекты
+        public static void InitComponents()
+        {
+            string localpath = Environment.CurrentDirectory + @"\Data\";
+            //SpellChecker.Init();
+            //SpellChecker.LoadDictionary(localpath + "SpChDict.dat");
+            //Associations.Init();
+            //Associations.LoadDictionary(localpath + "AssocDict.dat");
+            //Associations.LoadDictionaryBad(localpath + "AssocDictBad.dat");
+        }
+
+        // завершаем работы наших объектов
+        public static void CloseComponents()
+        {
+            //SpellChecker.SaveDictionary();
+            //Associations.SaveDictionary();
+            //Associations.SaveDictionaryBad();
+        }
+
+        // код основной программы
+        static void Main(string[] args)
+        {
+            // инитим лог
+            Log.Init();
+            Log.Write("________________________________________________________________________________");
+            Log.Write("      Старт программы..");
+            Log.Write("      Сборка от " + File.GetCreationTime(Process.GetCurrentProcess().MainModule.FileName).ToString());
+            Log.Write("      ПК: " + Environment.MachineName);
+            Log.Write("      " + System.Environment.OSVersion.VersionString + ", " + Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") + ", ver:" + Environment.Version.ToString() + ", CPU: " + Environment.ProcessorCount.ToString() + ", 64bit:" + Environment.Is64BitOperatingSystem.ToString());
+
+            // выполняем проверки окружения
+            if (!CheckComponents())
+            {
+                System.Windows.Forms.MessageBox.Show("Не все необхдимые компоненты установлены на ПК.\r\nПроверьте лог-файл.");
+                return;
+            }
+            // инициализируем наши собственные компоненты
+            InitComponents();
+
+            // создаём форму, передаём её управление
+            //MainForm MF1 = new MainForm();
+            //System.Windows.Forms.Application.Run(MainForm.MF);
+
+            //var tt = Upload.UploadFile_saveimgru(@"C:\1\34\pics\g24889_l2_p1_n1.jpg");
+
+            // закругляемся
+            CloseComponents();
+            Log.Write("Выход из программы..");
+            Log.Close();
         }
     }
 }
