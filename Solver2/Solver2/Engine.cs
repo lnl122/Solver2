5274c20bedb445ba3c160bf0e3db2566b4780acc
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-06 14:07:19 +0600

    add logon form, not tested

---
+++ Solver2/Solver2/Engine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver2
+{
+    // public static string Logon(string url1, string name, string pass) - выход - страница после попытки авторизации
+    //
+
+    class Engine
+    {
+        private static string cHead;            // куки
+        private static CookieContainer cCont;   // куки
+
+        // выполняем логон в движке
+        // вход - урл, логин, пасс
+        // выход - страница с ответом
+        public static string Logon(string url1, string name, string pass)
+        {
+            string formParams = string.Format("Login={0}&Password={1}", name, pass);
+            string cookieHeader = "";
+            var cookies = new CookieContainer();
+            cCont = cookies;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url1);
+            req.CookieContainer = cookies;
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(formParams);
+            req.ContentLength = bytes.Length;
+            using (Stream os = req.GetRequestStream()) { os.Write(bytes, 0, bytes.Length); }
+            string pageSource = "";
+            try
+            {
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                cookieHeader = resp.Headers["Set-cookie"];
+                cHead = cookieHeader;
+                using (StreamReader sr = new StreamReader(resp.GetResponseStream())) { pageSource = sr.ReadToEnd(); }
+            }
+            catch
+            {
+                Log.Write("en.cx ERROR: не удалось получить ответ на авторизацию", url1 + " " + name + " " + pass);
+            }
+            return pageSource;
+        }
+    }
+}
--- Solver2/Solver2/Logon.cs
+++ Solver2/Solver2/Logon.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Solver2
 {
+    // public Logon() - форма + авторизация. выход - структура класса
+    //
+
     class Logon
     {
+        private static int border = 5; // расстояния между элементами форм, константа
+        private static string url_game_en_cx = "http://game.en.cx/Login.aspx";
+
         public static bool isLogon;
         public static string username;
         public static string password;
@@ -15,7 +23,115 @@ namespace Solver2
 
         public Logon()
         {
+            // нужная ветка реестра д.б. в HKCU - //[HKEY_CURRENT_USER\Software\lnl122\solver] //"user"="username" //"pass"="userpassword"
+            // обратимся к реестру, есть ли там записи о последнем юзере, если есть - прочтем их
+            RegistryKey rk = Registry.CurrentUser;
+            RegistryKey rks = rk.OpenSubKey("Software", true); rk.Close();
+            RegistryKey rksl = rks.OpenSubKey("lnl122", true); if (rksl == null) { rksl = rks.CreateSubKey("lnl122"); }
+            rks.Close();
+            RegistryKey rksls = rksl.OpenSubKey("Solver", true); if (rksls == null) { rksls = rksl.CreateSubKey("Solver"); }
+            rksl.Close();
+            string user = "";
+            string pass = "";
+            var r_user = rksls.GetValue("user");
+            if (r_user == null) { rksls.SetValue("user", ""); user = ""; } else { user = r_user.ToString(); }
+            var r_pass = rksls.GetValue("pass");
+            if (r_pass == null) { rksls.SetValue("pass", ""); pass = ""; } else { pass = r_pass.ToString(); }
+            rksls.Close();
 
+            // форма для ввода данных
+            Form Login = new Form();
+            Login.Text = "Введите ник игрока и его пароль..";
+            Login.StartPosition = FormStartPosition.CenterScreen;
+            Login.Width = 35 * border;
+            Login.Height = 25 * border;
+            Login.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            Login.AutoSize = true;
+            Label lu = new Label();
+            lu.Text = "ник:";
+            lu.Top = 2 * border;
+            lu.Left = border;
+            lu.Width = 10 * border;
+            Login.Controls.Add(lu);
+            Label lp = new Label();
+            lp.Text = "пароль:";
+            lp.Top = lu.Bottom + border;
+            lp.Left = border;
+            lp.Width = lu.Width;
+            Login.Controls.Add(lp);
+            TextBox tu = new TextBox();
+            tu.Text = user;
+            tu.Top = lu.Top;
+            tu.Left = lu.Right + border;
+            tu.Width = 3 * lu.Width;
+            Login.Controls.Add(tu);
+            TextBox tp = new TextBox();
+            tp.Text = pass;
+            tp.Top = lp.Top;
+            tp.Left = tu.Left;
+            tp.Width = tu.Width;
+            Login.Controls.Add(tp);
+            Button blok = new Button();
+            blok.Text = "выполнить вход";
+            blok.Top = lp.Bottom + 2 * border;
+            blok.Left = lu.Left;
+            blok.Width = tu.Right - 1 * border;
+            blok.DialogResult = DialogResult.OK;
+            Login.AcceptButton = blok;
+            Login.Controls.Add(blok);
+
+            // предложим ввести юзера и пароль, дефолтные значения - то, что было в реестре, или же пусто
+            bool fl = true;
+            while (fl)
+            {
+                if (Login.ShowDialog() == DialogResult.OK)
+                {
+                    // попробуем авторизоваться на гейм.ен.цх с указанной УЗ
+                    user = tu.Text;
+                    pass = tp.Text;
+                    Log.Write("Пробуем выполнить вход на сайт для пользвоателя " + user);
+                    string pageSource = Engine.Logon(url_game_en_cx, user, pass);
+                    // если авторизовались успешно - записываем данные в реестр, меняем заголовок программы, делаем доступной кнорпку выбора игры
+                    if (pageSource.IndexOf("action=logout") != -1)
+                    {
+                        // обновить в реестре 
+                        RegistryKey rk2 = Registry.CurrentUser.OpenSubKey("Software\\lnl122\\Solver", true);
+                        rk2.SetValue("user", user);
+                        rk2.SetValue("pass", pass);
+                        rk2.Close();
+                        // изменим заголовок
+                        //MF.Text = mainform_caption + " / user: " + user;
+                        // запомним параметры игрока
+                        pageSource = pageSource.ToLower();
+                        pageSource = pageSource.Substring(pageSource.IndexOf(user.ToLower()));
+                        pageSource = pageSource.Substring(pageSource.IndexOf("(id"));
+                        pageSource = pageSource.Substring(pageSource.IndexOf(">") + 1);
+                        userid = pageSource.Substring(0, pageSource.IndexOf("<"));
+                        username = user;
+                        password = pass;
+                        isLogon = true;
+                        // поставим флаг выхода
+                        fl = false;
+                        // в лог
+                        Log.Write("Имя и пароль пользователя проверены, успешный логон для id=" + userid);
+                    }
+                    else
+                    {
+                        // если не успешно - вернемся в вводу пользователя
+                        userid = "";
+                        username = "";
+                        password = "";
+                        isLogon = false;
+                        Log.Write("Неверные логин/пароль");
+                        MessageBox.Show("Неверные логин/пароль");
+                    }
+                }
+                else
+                {
+                    // если отказались вводить имя/пасс - выходим
+                    fl = false;
+                }
+            } // выход только если fl = false -- это или отказ польователя в диалоге, или если нажато ОК - корректная УЗ
         }
     }
 }
