6592ebe4bcdce849430b51a3381321ce42c21f70
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-07 10:58:09 +0600

    _

--- Solver2/Solver2/Program.cs
+++ Solver2/Solver2/Program.cs
@@ -164,7 +164,13 @@ namespace Solver2
             if (!logonData.isSuccessful) { System.Windows.Forms.MessageBox.Show("Жаль, что не помните ник и пароль... :(\r\n\r\nМожет быть вам необходимо зарегистрироваться на ***.en.cx?"); }
             else
             {
-                System.Windows.Forms.MessageBox.Show("Едем!");
+                GameSelect GameSelectData = new GameSelect(logonData);
+                if (!GameSelectData.isSuccessful) { System.Windows.Forms.MessageBox.Show("Жаль, что не можете выбрать игру... :(\r\n\r\nМожет быть вам необходимо освежить память открыв список игр в браузере?"); }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Едем!");
+                    // *** тут нужно прочитать все уровни, заполнить инитные данные для формы, создать её и открыть.
+                }
             }
 
             // создаём форму, передаём её управление
