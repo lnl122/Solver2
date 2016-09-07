f25545d8d3f443504e47b6ff11e2559a2bde558a
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-09-07 18:11:34 +0600

    создана структура данных уровня

--- Solver2/Solver2/Levels.cs
+++ Solver2/Solver2/Levels.cs
@@ -1,9 +1,37 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 namespace Solver2
 {
     class Levels
     {
+        GameSelect Game;
+        public struct level
+        {
+            public int number;
+            public string name;
+            public string page;
+            public string text;
+            public bool isClose;
+            public List<string> answers_good;
+            public List<string> answers_bad;
+            public int sectors;
+            public int bonuses;
+            public string[] sector;
+            public string[] bonus;
+            public List<string> urls;
+            public string formlevelid;
+            public string formlevelnumber;
+            public DateTime dt;
+        }
 
+        public static level[] L;
+
+        public Levels(GameSelect GameParams)
+        {
+            Game = GameParams;
+        }
 
     }
 }
