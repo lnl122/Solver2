2bf63c0d37c6dfc8ad75e36b9e8e9a4573946815
adm <adm@NBIT01>
2016-09-24 03:30:53 +0600

    создана заглушка для www.hdkinoteatr.com

---
+++ Solver2/Solver2/Films.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver2
+{
+    class Films
+    {
+        // http://www.hdkinoteatr.com/
+        // и далее только через форму, она не простая
+        // надо бы тестировать в отдельном приложении с отдельным браузером в окне
+    }
+}
