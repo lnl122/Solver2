6c45dfbc842bee97d5a85e53d9c7f839a425887e
ovsyannikov.anton <ovsyannikov.anton@NBIT01>
2016-10-10 17:56:33 +0600

    правка using

--- Solver2/Solver2/WordCompare.cs
+++ Solver2/Solver2/WordCompare.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
+using Lstr = System.Collections.Generic.List<string>;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,13 @@ namespace Solver2
         // для двух наборов слов пробует решить метаграмму/логогрив/букарик
         // вход - два набора слов, два числа - суть количества букв, которые нужно менять в каждом из слов из наборов
         // выход - набор слов, соответствующий критериям
-        public List<string> Cmp(List<string> w1, List<string> w2, int i1, int i2)
+        public Lstr Cmp(Lstr w1, Lstr w2, int i1, int i2)
         {
             // для метаграммы - и1 = 1, и2 = 1
             // для логогрифа - и1 = 0, и2 = 1
             // для букарика - и1 = 1, и2 = 2
             // проверять нужно и w1-w2 и w2-w1
-            List<string> res = new List<string>();
+            Lstr res = new Lstr();
 
 
 
